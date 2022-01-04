2a0ba5766de5296163f1f7f1820885b0c2641b53
Nathan Cobb <nathanrcobb@gmail.com>
2022-01-04 14:49:39 -0800

    Added a spinner for loading

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,32 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 7,
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 4,
-    },
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const [feedback, setFeedback] = useState([])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Add feedback
   const addFeedback = (newFeedback) => {
@@ -60,6 +61,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         addFeedback,
         deleteFeedback,
         editFeedback,
